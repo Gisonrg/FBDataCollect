3901502542724c981b954752d0ce5e9e7e6e895a
Jiang Sheng <gisonrg@gmail.com>
2015-03-08 16:18:34 +0800

    Initial commit.

--- web/index.php
+++ web/index.php
@@ -10,11 +10,36 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
+// regester database
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
+
 // Our web handlers
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return 'Hello';
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->get('/twig/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name,
+    ));
 });
 
 $app->run();
