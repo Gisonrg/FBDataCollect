017e686f65c966f491193824750bf926754c5d32
Jiang Sheng <gisonrg@gmail.com>
2015-10-10 22:50:43 +0800

    Update mode.

--- web/index.php
+++ web/index.php
@@ -210,77 +210,77 @@ $app->get('/fb', function () use ($app, $helper, $config, $fb) {
             $friends = $response->getGraphEdge();
             $app['db']->executeQuery('UPDATE users SET no_friends = ? where id = ?', array($friends->getTotalCount(), $currentID));
             
-            $response = $fb->get('/me/likes', $token);
-            $likes = $response->getGraphEdge();
-            do {
-                foreach ($likes as $like) {
-                    $likedata = array(
-                        'userID' => $currentID,
-                        'category' => $like['category'],
-                        'name' => $like['name'],
-                        'created_time' => $like['created_time']->format('Y/m/d H:i:s')
-                    );
-                    try {
-                        $app['db']->insert('likes', $likedata);
-                    } catch (\Exception $e) {
-                        echo "DB ERROR likes!";
-                    }
-                }
-            } while ($likes = $fb->next($likes));
-            $response = $fb->get('/me/tagged_places', $token);
-            $places = $response->getGraphEdge();
-            do {
-                foreach ($places as $place) {
-                    $placeData = array(
-                        'userID' => $currentID,
-                        'city' => $place['place']['location']['city'],
-                        'country' => $place['place']['location']['country'],
-                        'latitude' => $place['place']['location']['latitude'],
-                        'longitude' => $place['place']['location']['longitude'],
-                        'name' => $place['place']['name'],
-                        'created_time' => $place['created_time']->format('Y/m/d H:i:s')
-                    );
-                    try {
-                        $app['db']->insert('place', $placeData);
-                    } catch (\Exception $e) {
-                        echo "DB ERROR place!";
-                    }
-                }
-            } while ($places = $fb->next($places));
+            // $response = $fb->get('/me/likes', $token);
+            // $likes = $response->getGraphEdge();
+            // do {
+            //     foreach ($likes as $like) {
+            //         $likedata = array(
+            //             'userID' => $currentID,
+            //             'category' => $like['category'],
+            //             'name' => $like['name'],
+            //             'created_time' => $like['created_time']->format('Y/m/d H:i:s')
+            //         );
+            //         try {
+            //             $app['db']->insert('likes', $likedata);
+            //         } catch (\Exception $e) {
+            //             echo "DB ERROR likes!";
+            //         }
+            //     }
+            // } while ($likes = $fb->next($likes));
+            // $response = $fb->get('/me/tagged_places', $token);
+            // $places = $response->getGraphEdge();
+            // do {
+            //     foreach ($places as $place) {
+            //         $placeData = array(
+            //             'userID' => $currentID,
+            //             'city' => $place['place']['location']['city'],
+            //             'country' => $place['place']['location']['country'],
+            //             'latitude' => $place['place']['location']['latitude'],
+            //             'longitude' => $place['place']['location']['longitude'],
+            //             'name' => $place['place']['name'],
+            //             'created_time' => $place['created_time']->format('Y/m/d H:i:s')
+            //         );
+            //         try {
+            //             $app['db']->insert('place', $placeData);
+            //         } catch (\Exception $e) {
+            //             echo "DB ERROR place!";
+            //         }
+            //     }
+            // } while ($places = $fb->next($places));
 
-            $response = $fb->get('/me/inbox', $token);
-            $messages = $response->getGraphEdge();
-            do {
-                $chatGroup = 0;
-                foreach ($messages as $message) {
-                    if (isset($message['comments'])) {
-                        foreach ($message['comments'] as $comment) {
-                            if (isset($comment['created_time'])) {
-                                $created_time = $comment['created_time']->format('Y/m/d H:i:s');
-                                $year = intval($comment['created_time']->format('Y'));
-                                if ($year < 2014) {
-                                    break;
-                                }
-                            }
-                            if (isset($comment['message'])) {
-                                if (isset($comment['from'])) {
-                                    $fromUser = $comment['from']['id'];
-                                    if ($fromUser == $user->getId()) {
-                                        $isFromUser = 1;
-                                    } else {
-                                        $isFromUser = 0;
-                                    }
-                                } else {
-                                    $fromUser = 'undefined';
-                                }
-                                $sql = "insert into messages(userID, postID, createTime, chatgroup, isFromUser, fromUser, content) values(".$currentID.", '".$comment['id']."', '".$created_time."', '".$chatGroup."', '".$isFromUser."', '".$fromUser."', '".htmlspecialchars($comment['message'], ENT_QUOTES)."')";
-                                $app['db']->query($sql);
-                            }
-                        }
-                        $chatGroup++;
-                    }
-                }
-            } while ($messages = $fb->next($messages));
+            // $response = $fb->get('/me/inbox', $token);
+            // $messages = $response->getGraphEdge();
+            // do {
+            //     $chatGroup = 0;
+            //     foreach ($messages as $message) {
+            //         if (isset($message['comments'])) {
+            //             foreach ($message['comments'] as $comment) {
+            //                 if (isset($comment['created_time'])) {
+            //                     $created_time = $comment['created_time']->format('Y/m/d H:i:s');
+            //                     $year = intval($comment['created_time']->format('Y'));
+            //                     if ($year < 2014) {
+            //                         break;
+            //                     }
+            //                 }
+            //                 if (isset($comment['message'])) {
+            //                     if (isset($comment['from'])) {
+            //                         $fromUser = $comment['from']['id'];
+            //                         if ($fromUser == $user->getId()) {
+            //                             $isFromUser = 1;
+            //                         } else {
+            //                             $isFromUser = 0;
+            //                         }
+            //                     } else {
+            //                         $fromUser = 'undefined';
+            //                     }
+            //                     $sql = "insert into messages(userID, postID, createTime, chatgroup, isFromUser, fromUser, content) values(".$currentID.", '".$comment['id']."', '".$created_time."', '".$chatGroup."', '".$isFromUser."', '".$fromUser."', '".htmlspecialchars($comment['message'], ENT_QUOTES)."')";
+            //                     $app['db']->query($sql);
+            //                 }
+            //             }
+            //             $chatGroup++;
+            //         }
+            //     }
+            // } while ($messages = $fb->next($messages));
 
             $before2013 = false;
             $response = $fb->get('/me/posts', $token);
